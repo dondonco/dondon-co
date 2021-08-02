3932f98048647848cbbecd0808e737411c8286e5
Dondon Co <dondon.co19@gmail.com>
2021-08-02 12:46:23 -0700

    made changes to the portfolio

--- src/Home.js
+++ src/Home.js
@@ -9,9 +9,14 @@ const Home = () => {
               
                 </div>
                 <div className="container__right">
-                    <p className="greetings">hi i'm Dondon</p>
-                    <p>I created this website to show that I have the basic skills and knowledge to start working into the tech industry. I really would loved to start my journey professionally in tech field and I hope that you would help me achieve this current goal of mine. Thank you for taking your time in browsing my website and I hope that I would hear from you!</p>
-                    <p>This web app is currently being hosted on githube</p>
+                    <h1 className="greetings">hi i'm Dondon</h1>
+                    <p>
+                        This website was created using react. It is currently being hosted on github. I'm still in the process of developmet this website while I'm learning and creating projects for my portfolio.
+                    </p>
+                    <p>
+                        I created this website to show that I have the basic skills and knowledge to start working into the tech industry. I really would loved to start my journey professionally in tech field and I hope that you would help me achieve this current goal of mine. Thank you for taking your time in browsing my website and I hope that I would hear from you!
+                    </p>
+                    
                 </div>
             </div>
         </div>
